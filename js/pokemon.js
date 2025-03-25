532458033579522c458cb8edfbc5b58a24ae85ff
Iederees Francis <63100527+iederees-create@users.noreply.github.com>
2025-03-25 21:32:24 +0200

    Update pokemon.js

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,6 +1,6 @@
 // Pok‚mon data and functions 
 const POKEMON_LIST = [
-    { name: "Pikachu", image: "assets/images/pikachu/pikachu_1.png", hp: 100, attack: 15 },
+    { name: "Pikachu", image: "https://github.com/iederees-create/pokemon-battle-simulator/blob/501c3c1fd69f9355394cc13ba77650c8bf7a0dcf/assets/images/pikachu/pikachu_1.png", hp: 100, attack: 15 },
     { name: "Charizard", image: "charizard.png", hp: 120, attack: 20 },
     { name: "Bulbasaur", image: "bulbasaur.png", hp: 90, attack: 10 }
 ];
