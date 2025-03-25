63d21910352fab3ae67ce07bbf802a1edd1a2b98
Iederees Francis <63100527+iederees-create@users.noreply.github.com>
2025-03-25 21:16:27 +0200

    Update pokemon.js

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,1 +1,18 @@
-// Pok‚mon data and functions 
+// Pokâ€šmon data and functions 
+const POKEMON_LIST = [
+    { name: "Pikachu", image: "pikachu.png", hp: 100, attack: 15 },
+    { name: "Charizard", image: "charizard.png", hp: 120, attack: 20 },
+    { name: "Bulbasaur", image: "bulbasaur.png", hp: 90, attack: 10 }
+];
+
+function loadPokemonImages() {
+    return POKEMON_LIST.map(pokemon => {
+        return {
+            ...pokemon,
+            imagePath: `assets/images/${pokemon.image}`
+        };
+    });
+}
+
+const pokemonData = loadPokemonImages();
+console.log(pokemonData); // Check if images are loaded
